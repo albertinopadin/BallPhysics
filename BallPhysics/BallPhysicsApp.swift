4837c825135d5253094a6d31184930c9be2bed96
Albertino Padin <albertino.padin@gmail.com>
2024-10-08 00:52:02 -0700

    Initial Commit

---
+++ BallPhysics/BallPhysicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallPhysicsApp.swift
+//  BallPhysics
+//
+//  Created by Albertino Padin on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BallPhysicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BallPhysicsTests/BallPhysicsTests.swift
@@ -0,0 +1,16 @@
+//
+//  BallPhysicsTests.swift
+//  BallPhysicsTests
+//
+//  Created by Albertino Padin on 10/8/24.
+//
+
+import Testing
+
+struct BallPhysicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
